8adde35c5d671f3e20fdef4acb04395eb53bbcc8
AndreyBychenkow <decebell032@yandex.ru>
2024-10-24 23:27:13 +0300

    add search to admin UI

--- real_estate_agency/settings.py
+++ real_estate_agency/settings.py
@@ -9,6 +9,9 @@ env.read_env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
 SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
