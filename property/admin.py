85089556bab4c34f1847ec21ed457fd5dbaa90e3
AndreyBychenkow <decebell032@yandex.ru>
2024-10-25 16:20:50 +0300

    setup admin UI for likes

--- property/admin.py
+++ property/admin.py
@@ -11,6 +11,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
 
     list_filter = ['new_building', 'town', 'construction_year', 'active']
+    raw_id_fields = ['likes']
 
 
 class ComplaintAdmin(admin.ModelAdmin):
